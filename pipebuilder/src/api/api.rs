f4725f45a7d8d2581751547cace112fee33bdd45
Li Yu <li.yu.sh0211@gmail.com>
2021-11-01 15:02:06 -0700

    filter rename

--- pipebuilder/src/api/api.rs
+++ pipebuilder/src/api/api.rs
@@ -18,21 +18,21 @@ pub mod filters {
         register: Register,
         lease_id: i64,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        app(repository_client.clone(), register.clone())
-            .or(build(scheduler_client, register.clone(), lease_id))
-            .or(manifest(repository_client, register.clone()))
-            .or(namespace(register.clone(), lease_id))
-            .or(node(register.clone(), lease_id))
-            .or(project(register, lease_id))
+        v1_app(repository_client.clone(), register.clone())
+            .or(v1_build(scheduler_client, register.clone(), lease_id))
+            .or(v1_manifest(repository_client, register.clone()))
+            .or(v1_namespace(register.clone(), lease_id))
+            .or(v1_node(register.clone(), lease_id))
+            .or(v1_project(register, lease_id))
     }
 
     // build api
-    pub fn build(
+    pub fn v1_build(
         scheduler_client: SchedulerClient<Channel>,
         register: Register,
         lease_id: i64,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        v1_build(scheduler_client, register.clone())
+        v1_build_post(scheduler_client, register.clone())
             .or(v1_build_snapshot_list(register.clone()))
             .or(v1_build_get(register.clone(), lease_id))
             .or(v1_build_list(register.clone()))
@@ -43,7 +43,7 @@ pub mod filters {
     }
 
     // manifest api
-    pub fn manifest(
+    pub fn v1_manifest(
         repository_client: RepositoryClient<Channel>,
         register: Register,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -55,7 +55,7 @@ pub mod filters {
     }
 
     // app api
-    pub fn app(
+    pub fn v1_app(
         repository_client: RepositoryClient<Channel>,
         register: Register,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -65,7 +65,7 @@ pub mod filters {
     }
 
     // namespace api
-    pub fn namespace(
+    pub fn v1_namespace(
         register: Register,
         lease_id: i64,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -73,14 +73,14 @@ pub mod filters {
     }
 
     // namespace api
-    pub fn project(
+    pub fn v1_project(
         register: Register,
         lease_id: i64,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         v1_project_put(register.clone(), lease_id).or(v1_project_list(register))
     }
 
-    pub fn node(
+    pub fn v1_node(
         register: Register,
         lease_id: i64,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -89,7 +89,7 @@ pub mod filters {
             .or(v1_node_deactivate(register, lease_id))
     }
 
-    pub fn v1_build(
+    pub fn v1_build_post(
         scheduler_client: SchedulerClient<Channel>,
         register: Register,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
